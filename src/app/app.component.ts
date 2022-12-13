43b35da5ca89800fd7e74a05c4b16054ce1f5951
yogesan66 <105202046+yogesan66@users.noreply.github.com>
2022-12-13 23:32:40 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  /*animals=["tiger","lion","fox"]
+  animalname=""*/
+
+  studentname=""
+  studentnames=["ram","yogesh","gokul","hari","vignesh"]
+}
